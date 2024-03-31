af65ce85656f665b794fdfc7c65b92f1f60bdf1b
sugahikaru <sh09054713265@outlook.jp>
2024-03-31 17:43:57 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("nana", 17);
+        Dog dog2 = new Dog("rara", 22);
+
+        List<Dog> dogList = new ArrayList<>();
+        dogList.add(dog1);
+        dogList.add(dog2);
+
+        dog1.setName("koko");
+        
+
+        System.out.println(dog1.getName());
+    }
+}
